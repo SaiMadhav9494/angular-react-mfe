7183ffc7e4929dafbd33d0f2c1f94e096af87a63
Kevin Logan <kevin.logan@ravenind.com>
2023-03-22 08:54:57 -0500

    Show which framework you are in

---
+++ projects/shell/src/app/hello/hello.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  templateUrl: './hello.component.html',
+  styles: [
+  ]
+})
+export class HelloComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
